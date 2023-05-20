d0bd5b051e8334883546aa71509794d1310c1994
ClaudiaCristinaTomus <tomus_clau@yahoo.com>
2023-05-20 16:31:05 +0300

    navbar

---
+++ src/Navbar/Navbar.js
@@ -0,0 +1,48 @@
+import React,{useState} from 'react'
+import "./Navbar.scss"
+import {DiJqueryLogo} from 'react-icons/di'
+import {FiMenu} from 'react-icons/fi'
+import {GrClose} from "react-icons/gr"
+
+const Navbar = () => {
+    const [click,setClicked]=useState(false);
+    const [showNav,setShowNav] = useState(false);
+
+    const handleClick = () =>{
+        setClicked(!click);
+    }
+    const handleCloseMobileMenu=()=>{
+        setShowNav(!click);
+    }
+
+    return (
+       
+            <nav className={showNav ? "navbar-container": "navbar-container active"}>
+                <a href="/" className="navbar-logo">
+                    <DiJqueryLogo/>
+                
+                </a>
+                <div className="menu-icon" onClick={handleClick}>
+                    {click ? <GrClose/> :<FiMenu/>}
+                   
+                </div>
+                <ul className={click ? "nav-menu active" : "nav-menu"}>
+                    <li className="nav-link-items" onClick={handleCloseMobileMenu}>
+                        <a href="/" className="nav-link">Home</a>
+                    </li>
+                    <li className="nav-link-items" onClick={handleCloseMobileMenu}>
+                        <a href="/" className="nav-link">About</a>
+                    </li>
+                    <li className="nav-link-items" onClick={handleCloseMobileMenu}>
+                        <a href="/" className="nav-link">Portfolio</a>
+                    </li>
+                    <li className="nav-link-items" onClick={handleCloseMobileMenu}>
+                        <a href="/" className="nav-link">Contact</a>
+                    </li>
+                </ul>
+            </nav>
+      
+    )
+}
+
+export default Navbar
